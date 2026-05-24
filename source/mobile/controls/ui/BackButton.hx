b9a967f75e2de27f3fcf7538523f0dcb8d11e5d1
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-24 15:32:24 -0700

    fixes

--- source/mobile/controls/ui/BackButton.hx
+++ source/mobile/controls/ui/BackButton.hx
@@ -5,51 +5,60 @@ import flixel.FlxSprite;
 import flixel.FlxCamera;
 import flixel.input.keyboard.FlxKey;
 import funkin.backend.assets.Paths;
-
+import flixel.input.mouse.FlxMouse;
+#if mobile
 class BackButton extends FlxSprite {
-    
-    public static var isPressingBack:Bool = false;
-    var canClick:Bool = true;
 
-    public function new(x:Float = 975, y:Float = 455, ?buttonCam:FlxCamera) {
-        super(x, y);
+	public static var isPressingBack:Bool = false;
 
-        frames = Paths.getSparrowAtlas('backButton');
-        
-        animation.addByPrefix('idle', 'back0000', 24, true);
-        animation.addByPrefix('clicked', 'back', 24, false);
-        animation.play('idle');
-        
-        scale.set(0.85, 0.85);
-        updateHitbox();
-        
-        scrollFactor.set(0, 0); 
-        
-        if (buttonCam != null) {
-            this.cameras = [buttonCam];
-        }
-    }
+	public function new(x:Float = 975, y:Float = 455, ?buttonCam:FlxCamera) {
+		super(x, y);
 
-    override public function update(elapsed:Float):Void {
-        super.update(elapsed);
+		frames = Paths.getSparrowAtlas('backButton');
 
-        if (canClick && FlxG.mouse.overlaps(this, this.camera) && FlxG.mouse.justPressed) {
-            canClick = false;
-            isPressingBack = true;
-            animation.play('clicked', true);
-            
-            FlxG.keys.handleAction(FlxKey.BACKSPACE, true);
-        }
+		animation.addByPrefix('idle', 'back0000', 24, true);
+		animation.addByPrefix('clicked', 'back', 24, false);
 
-        if (isPressingBack && FlxG.mouse.justReleased) {
-            isPressingBack = false;
-            
-            FlxG.keys.handleAction(FlxKey.BACKSPACE, false);
-        }
+		animation.play('idle');
 
-        if (animation.curAnim != null && animation.curAnim.name == 'clicked' && animation.curAnim.finished) {
-            animation.play('idle');
-            canClick = true; 
-        }
-    }
+		scale.set(0.85, 0.85);
+		updateHitbox();
+
+		scrollFactor.set(0, 0);
+
+		if (buttonCam != null)
+			cameras = [buttonCam];
+	}
+
+	override public function update(elapsed:Float):Void {
+		super.update(elapsed);
+
+		var hovering:Bool = FlxG.mouse.overlaps(this, this.camera);
+
+		if (hovering)
+			FlxMouse.globallyBlocked = true;
+
+		if (hovering && FlxG.mouse.justPressed) {
+			isPressingBack = true;
+
+			animation.play('clicked', true);
+
+			FlxG.keys.handleAction(FlxKey.BACKSPACE, true);
+		}
+
+		if (isPressingBack && FlxG.mouse.justReleased) {
+			isPressingBack = false;
+
+			animation.play('idle');
+
+			FlxG.keys.handleAction(FlxKey.BACKSPACE, false);
+		}
+
+		if (animation.curAnim != null
+			&& animation.curAnim.name == 'clicked'
+			&& animation.curAnim.finished) {
+			animation.play('idle');
+		}
+	}
 }
+#end
