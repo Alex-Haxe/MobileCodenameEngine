1b0c2f211ea9a015a4e9e72b12bde68fc45ce11e
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-05-24 16:05:31 -0700

    ....

--- source/mobile/controls/ui/BackButton.hx
+++ source/mobile/controls/ui/BackButton.hx
@@ -18,53 +18,55 @@ class BackButton extends FlxSprite {
 		if (buttonCam == null) {
 			buttonCam = new FlxCamera();
 			buttonCam.bgColor.alpha = 0;
-
 			FlxG.cameras.add(buttonCam, false);
 		}
 
 		cameras = [buttonCam];
-
 		frames = Paths.getSparrowAtlas('backButton');
 
 		animation.addByPrefix('idle', 'back0000', 24, true);
-		animation.addByPrefix('clicked', 'back', 24, false);
+		animation.addByPrefix('clicked', 'back pressed', 24, false);
 
 		animation.play('idle');
-
 		scale.set(0.85, 0.85);
-
 		updateHitbox();
-
 		scrollFactor.set(0, 0);
 	}
 
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 
-		var hovering:Bool = FlxG.mouse.overlaps(this, cameras[0]);
+		var hovering:Bool = false;
+		var pointerJustPressed:Bool = false;
+		var pointerJustReleased:Bool = false;
+
+		if (FlxG.mouse.overlaps(this, cameras[0])) {
+			hovering = true;
+			if (FlxG.mouse.justPressed) pointerJustPressed = true;
+		}
+		
+		if (FlxG.mouse.justReleased) pointerJustReleased = true;
+
+		for (touch in FlxG.touches.list) {
+			if (touch.overlaps(this, cameras[0])) {
+				hovering = true;
+				if (touch.justPressed) pointerJustPressed = true;
+			}
+			if (touch.justReleased) pointerJustReleased = true;
+		}
 
 		FlxMouse.globallyBlocked = hovering;
 
-		if (hovering && FlxG.mouse.justPressed) {
+		if (hovering && pointerJustPressed) {
 			isPressingBack = true;
-
 			animation.play('clicked', true);
-
 			FlxG.keys.handleAction(FlxKey.BACKSPACE, true);
 		}
 
-		if (isPressingBack && FlxG.mouse.justReleased) {
+		if (isPressingBack && pointerJustReleased) {
 			isPressingBack = false;
-
 			animation.play('idle');
-
 			FlxG.keys.handleAction(FlxKey.BACKSPACE, false);
-		}
-
-		if (animation.curAnim != null
-			&& animation.curAnim.name == 'clicked'
-			&& animation.curAnim.finished) {
-			animation.play('idle');
 		}
 	}
 }
