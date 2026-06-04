e16a59a1b9f91324f9d2a204bd9fb34cefa300f1
LuckyGuy <261809561+ChanceXML@users.noreply.github.com>
2026-06-03 20:23:21 -0700

    Refactor logging to use FlxCamera and Haxe Log

--- assets/data/scripts/trace.hx
+++ assets/data/scripts/trace.hx
@@ -3,31 +3,32 @@ import flixel.text.FlxTextBorderStyle;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxCamera;
+import funkin.backend.assets.Paths;
+import haxe.Log;
 
 var logText:FlxText;
 var logHistory:Array<String> = [];
 var maxLogLines:Int = 15;
+var myLogCam:FlxCamera;
 
 #if mobile
 function postCreate() {
-    camOther = new FlxCamera();
-    camOther.bgColor = 0x00000000;
-    FlxG.cameras.add(camOther, false);
+    myLogCam = new FlxCamera();
+    myLogCam.bgColor = 0x00000000;
+    FlxG.cameras.add(myLogCam, false);
   
     logText = new FlxText(10, 10, FlxG.width - 20, "", 16);
-    
-    logText.setFormat(null, 16, FlxColor.WHITE, "left", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+    logText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, "left", FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
     logText.scrollFactor.set(0, 0); 
+    logText.cameras = [myLogCam];
     
-    logText.cameras = [camOther];
-    
-    add(logText);
+    FlxG.state.add(logText);
 }
 
-function logScreen(text:Dynamic) {
+function trace(text:Dynamic) {
     var msg:String = Std.string(text);
     
-    trace(msg);
+    Log.trace(msg);
     
     logHistory.push(msg);
     
